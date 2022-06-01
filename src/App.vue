51cd56b0a563a6451849996f305ece24a8c5cd06
jhordanv10 <jhordanvilla1000@gmail.com>
2022-06-01 10:00:20 -0500

    Creaci+on y configuración entorno de desarrollo

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,58 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+      <v-btn
+        v-for="{id, rute, name, icon} in rutes"
+        :key="id"
+        @click="selectRute(rute)"
+      >
+        <span class="mr-2">{{ name }} </span>
+        <v-icon>{{ icon }}</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+export default {
+  name: "App",
+  data: () => ({
+    rutes: [
+      { id: 1, name: "home", rute: "/", icon:"mdi-home" },
+      { id: 1, name: "films", rute: "/films", icon:"mdi-video" },
+      { id: 1, name: "starships", rute: "/starships", icon:"mdi-star" },
+      { id: 1, name: "vehicles", rute: "/vehicles", icon:"mdi-car" },
+    ],
+  }),
+  methods: {
+    selectRute(rute) {
+      this.$router.push(rute);
+    },
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import FilmsView from '../views/FilmsView.vue'
+import StarShipsView from '../views/StarShipsView.vue'
+import VehiclesView from '../views/VehiclesView.vue'
 
 Vue.use(VueRouter)
 
@@ -11,15 +14,21 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
-  }
+    path: '/films',
+    name: 'films',
+    component: FilmsView
+  },
+  {
+    path: '/starships',
+    name: 'starships',
+    component: StarShipsView
+  },
+  {
+    path: '/vehicles',
+    name: 'vehicles',
+    component: VehiclesView
+  },
+  
 ]
 
 const router = new VueRouter({
